bf83f77e9fee597d6c0b5043097248102f871e65
Gaurav Mishra <gaurav.info217@gmail.com>
2023-08-05 00:42:10 +0530

    Updated auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,45 @@
 const User = require('../model/User');
+const validateAge = require('../utils/ageValidation');
 
-exports.signup = () => {};
+exports.signup = async(req,res,next) => {
+    try{
+        const {firstName,lastName,userName,email,phone,dateofbirth} = req.body;
+        
+        if(!firstName || !lastName || !userName || !email || !phone || !dateofbirth){
+            return res.status(400).json({
+                status : 'Fail',
+                message : 'Missing field data'
+            });
+        }
+
+        if(validateAge(dateofbirth) == false){
+            return res.status(400).json({
+                status : 'Fail',
+                message : 'Invalid Age'
+            })
+        }
+
+        const newUser = await User.create({
+            firstName : firstName,
+            lastName : lastName,
+            userName : userName,
+            email : email,
+            phone : phone,
+            dateofBirth : dateofbirth
+        })
+
+        res.status(201).json({
+            status : 'Success',
+            newUser
+        })
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({
+            status : 'Fail',
+            message : 'Internal Server Error'
+        });
+    }
+};
 exports.login = () => {};
-
