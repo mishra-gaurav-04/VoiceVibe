ba74bd2d11fceae1d52a46f9c008da94cee15f5a
Gaurav Mishra <gaurav.info217@gmail.com>
2023-08-09 00:54:25 +0530

    updated authSlice

--- client/src/redux/authSlice.js
+++ client/src/redux/authSlice.js
@@ -13,8 +13,11 @@ export const authSlice = createSlice({
     name : 'auth',
     initialState,
     reducers : {
-        setAuth : (state,action) => {},
-        
+        setAuth : (state,action) => {
+            const {user,auth} = action.payload;
+            state.user = user;
+            state.isAuth = auth;
+         },
         setOtp : (state,action) => {
             const {email,hash} = action.payload;
             state.otp.email = email;
